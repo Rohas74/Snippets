f95c9535cf50dc5bff1a9bb1fdcbf52127994a8c
Rohas74 <rohas74@yandex.ru>
2024-07-24 20:16:34 +0300

    Day 3 Snippets Step 3

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,8 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from django.db import models
-
+from MainApp.models import Snippet
+from django.core.exceptions import ObjectDoesNotExist
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -14,5 +15,16 @@ def add_snippet_page(request):
 
 
 def snippets_page(request):
-    context = {'pagename': 'Просмотр сниппетов'}
+    snipp = Snippet.objects.all()
+    context = {"items": snipp,}
     return render(request, 'pages/view_snippets.html', context)
+
+def view_curr_snippet(request, item_id):
+    try:
+        cur_snip = Snippet.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        return render(request, "errors.html", {"error": f"Item with id={item_id} not found."})
+    else:
+        context = {"items": cur_snip,}
+        return render(request, 'pages/view_snippet.html', context)
+
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index_page, name="main_page"),
     path('add_snippet', views.add_snippet_page, name="add_snip_page"),
     path('view_snippets', views.snippets_page, name="view_snip"),
+    path('view_snipp/<int:item_id>', views.view_curr_snippet, name="snip_detail"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
